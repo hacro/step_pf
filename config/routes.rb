ebbf4db239617e5fb184b8fc8690f5cfdd434c0a
hazuki <87082685+hacro@users.noreply.github.com>
2023-11-21 12:48:04 +0900

    いいね機能の実装（同期処理）

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,14 @@ Rails.application.routes.draw do
   get "/users/mypage" => "users#mypage", as: "mypage"
   get "/users/:id/unsubscribe" => "users#unsubscribe", as: "unsubscribe"
   patch "/users/:id/withdrawal" => "users#withdrawal", as: "withdrawal"
+  # post "/favorite/:id" => "favorites#create", as: "create_favorite"
+  # delete "/favorite/:id" => "favorites#destroy", as: "destroy_favorite"
   resources :users, only: [:show, :edit, :update]
-  
-  resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-
+  resources :favorites, only: [:create, :destroy]
+  # resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :posts do
+    resource :favorites, only: [:create, :destroy]
+  end
   get "home/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
