7f1479913ad4d2e75992827230547be5d227acf4
hazuki <87082685+hacro@users.noreply.github.com>
2023-12-11 11:18:09 +0900

    レスポンシブ対応

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,22 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+
+// class MobileMenu{
+//     constructor(){
+//         this.DOM = {};
+//         this.DOM.btn = document.querySelector('.mobile-menu__btn');
+//         this.DOM.container = document.querySelector('#global-container');
+//         this.DOM.btn.addEventListener('click', this._toggle.bind(this));
+//     }
+
+//     _toggle(){
+//         this.DOM.container.classList.toggle('menu-open');
+//     }
+// }
+
+// new MobileMenu();
+
 $(function(){
     $('.about-comment').on('click', () => {
         $('.modal').css('display','flex');
